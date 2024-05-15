d8e1a408d7770b0352e50f897e9752678be525ca
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-05-16 00:58:26 +0300

    Added material ui packages and docs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
+
+import theme from './theme.tsx';
+import store from './redux/store.ts';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.Fragment>
+    <ThemeProvider theme={theme}>
+        <Provider store={store}>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+        </Provider>
+    </ThemeProvider>
+  </React.Fragment>,
 )
